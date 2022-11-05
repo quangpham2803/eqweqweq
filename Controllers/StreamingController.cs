3e2af6588c08ce6b25e0f8ed5b1ab6645bfa317c
quangpham2803 <quangpham2803@gmail.com>
2022-11-05 13:46:36 +0700

    thứ 7 5/11

---
+++ Controllers/StreamingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace hentaweb_v2.Controllers
+{
+    public class StreamingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
