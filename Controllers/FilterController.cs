49b2d9fd3e5321b9a18831d1b79e975ea3e1c522
quangpham2803 <quangpham2803@gmail.com>
2022-11-09 14:16:24 +0700

    admin filter

---
+++ Controllers/FilterController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace hentaweb_v2.Controllers
+{
+    public class FilterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
